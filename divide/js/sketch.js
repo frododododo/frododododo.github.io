8edcee12c5eddfd43a7b404c2cde5c4888d8169d
frododododo <frododo123@gmail.com>
2020-10-06 14:00:54 +0200

    divide and conquer epic

---
+++ divide/js/sketch.js
@@ -0,0 +1,48 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  
+
+
+  // put setup code here
+
+//blokken
+  //eerste rij
+ 
+  fill(199)
+  rect(0,0,width/4,height/3);
+  rect(width/4*1,0,width/4,height/3);
+  fill(252, 107, 3)
+  rect(width/4*2,0,width/4,height/3);
+  fill(199)
+  rect(width/4*3,0,width/4,height/3);
+
+  //tweede rij
+  fill(199)
+  rect(width/4*0,height/3*1,width/4,height/3);
+  fill(252, 107, 3)
+  rect(width/4*1,height/3*1,width/4,height/3);
+  fill(199)
+  rect(width/4*2,height/3*1,width/4,height/3);
+  fill(199)
+  rect(width/4*3,height/3*1,width/4,height/3);
+
+  //derde rij
+  fill(199)
+  rect(width/4*0,height/3*2,width/4,height/3);
+  fill(199)
+  rect(width/4*1,height/3*2,width/4,height/3);
+  fill(199)
+  rect(width/4*2,height/3*2,width/4,height/3);
+  fill(252, 107, 3)
+  rect(width/4*3,height/3*2,width/4,height/3);
+  
+
+
+  
+  
+}
+
+function draw() {
+  // put drawing code here
+  
+}
