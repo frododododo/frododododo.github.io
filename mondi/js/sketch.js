a2f40fba5a0c84fb35b0f110881435f21a9f723a
frododododo <frododo123@gmail.com>
2020-10-06 14:07:49 +0200

    monderiaa

---
+++ mondi/js/sketch.js
@@ -0,0 +1,63 @@
+function setup() {
+  createCanvas(400,400);
+  background(220);
+  // put setup code here
+  
+
+//rect zwart links boven
+  strokeWeight(5)
+  fill(10);
+  rect(0,0,35,125);
+
+  //licht grijs rect
+  fill(225);
+  rect(0,125,35,150);
+
+  //groter vak bijna links onderin 90 breed
+  fill(190);
+  rect(0,275,90,70);
+
+  //laatste linkervlak
+  fill(225);
+  rect(0,345,90,55)
+
+  //rood vlak er naast
+  fill(235,64,52)
+  rect(90,275,90,90)
+
+  //zwart valk daarboven
+  fill(10)
+  rect(35,200,145,75)
+
+  //blauw vlak twee daarboven
+  fill(50,62,168)
+  rect(35,125,145,75)
+
+  //linksboven grijs 
+  fill(225)
+  rect(35,0,145,35)
+
+  //linksboven rood
+  fill(235,64,52)
+  rect(35,35,65,90)
+
+  //linksboven licht
+  fill(255)
+  rect(100,35,80,90)
+
+  //zwart vlak helemaal onderin plat
+  //fill(10)
+  //rect(90,365,)
+
+
+  ///////////rects//////////////
+
+
+
+}
+
+function draw() {
+  // put drawing code here
+
+
+}
